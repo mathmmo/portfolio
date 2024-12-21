7b1471ba4b4c03e58377816d478bca9ebea48c70
mathmmo <matheusm.oliveira1995@gmail.com>
2024-12-21 03:23:03 -0300

    hello world hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/Hero";
-import { main } from "framer-motion/client";
+// import { main } from "framer-motion/client";
 
 
 export default function Home() {
@@ -7,14 +7,7 @@ export default function Home() {
     // <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 h-full">
     <main className="bg-black-100">
       <div className="w-full h-full">
-        {/* <Hero /> */}
-        <div className="flex flex-col items-center justify-center min-h-screen bg-black-100 text-gray-300 w-full">
-          <h1 className="text-4xl font-bold mb-4">Hello, World!</h1>
-          <p className="text-lg">
-            This portfolio is a <span className="font-semibold text-blue-500">work in progress</span>. Stay tuned!
-          </p>
-          <small className="text-gray-500">By: Matheus Oliveira</small>
-        </div>
+        <Hero />
       </div>
     </main>
   );
