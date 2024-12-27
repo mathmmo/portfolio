5354d2e6cdf24cef80b33ab92570bec9c672cbba
mathmmo <matheusm.oliveira1995@gmail.com>
2024-12-27 19:19:02 -0300

    Third deploy, with recent projects section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,11 +8,11 @@ import { FaLocationArrow } from 'react-icons/fa6'
 const Hero = () => {
     return (
         <div className="pb-20 pt-36 ">
-            <div>
-                <Spotlight className="-top-40 -left-10 md:-top-20 md:-left-32 h-screen" fill='white' />
-                <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill='white' />
-                <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill='white' />
-            </div>
+
+            <Spotlight
+                className="absolute top-0 right-0 w-1/3 h-1/3 transform translate-x-1/2 -translate-y-1/2 -rotate-45 md:w-1/4 md:h-1/4"
+                fill="white"
+            />
             <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
             </div>
@@ -21,12 +21,12 @@ const Hero = () => {
                     <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Math&apos;s Portfolio</h2>
                     
                     <TextGenerateEffect
-                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                        words="You know the Business"
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl mb-0"
+                        words="You Know Business"
                     />
                     <TextGenerateEffect
                         className="text-center text-[40px] md:text-5xl lg:text-6xl mt-0 pt-0 mb-6"
-                        words="I know the chemistry"
+                        words="I know Chemistry"
                         textColor='text-[#a784f9]'
                     />
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,7 +1,9 @@
 import { projects } from '@/data'
 import React from 'react'
-import { PinContainer } from './ui/3d-pin'
+// import { PinContainer } from './ui/3d-pin'
 import { FaLocationArrow } from 'react-icons/fa6'
+import { CardContainer, CardBody, CardItem } from './ui/3d-card'
+import Image from 'next/image'
 
 const RecentProjects = () => {
   return (
@@ -10,7 +12,7 @@ const RecentProjects = () => {
             A small selection of {' '}
             <span className='text-[#a784f9]'>recent projects</span>
         </h1>
-        <div className='flex flex-wrap items-center justify-center gap-x-24 gap-y-8 pt-4 mt-10'>
+        {/* <div className='flex flex-wrap items-center justify-center gap-x-24 gap-y-8 pt-4 mt-10'>
             {projects.map(({id, title, des, img, iconLists, link}) => (
                 <div key={id} className='sm:h-[41rem] lg:min-h-[32.5rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                     <PinContainer title={link} href={link}>
@@ -47,6 +49,68 @@ const RecentProjects = () => {
                             </div>
                         </div>
                     </PinContainer>
+                </div>
+            ))}
+        </div> */}
+        <div className='flex flex-wrap items-center justify-center gap-x-24 gap-y-8 pt-4 mt-8'>
+            {projects.map(({id, title, des, img, iconLists, link}) => (
+                <div key={id} className='sm:h-[41rem] lg:min-h-[32.5rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
+                    <CardContainer>
+                        <CardBody className="bg-black-100 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[570px] w-[80vw] h-auto rounded-xl p-6 border border-w/[0.2]">
+                            <CardItem translateZ={50} translateY={-5} className='mb-4'>
+                                <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>{title}</h1>
+                            </CardItem>
+                            <CardItem translateZ={50} className='mb-12'>
+                                <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>{des}</p>
+                            </CardItem>
+                            <CardItem
+                                translateZ={100}
+                                // rotateX={40}
+                                rotateZ={-5}
+                                className="w-full mt-4"
+                                >
+                                <Image
+                                    src={img}
+                                    height="1000"
+                                    width="1000"
+                                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                    alt={img}
+                                />
+                            </CardItem>
+                            <div className="flex justify-between items-center mt-10">
+                                <CardItem
+                                    translateZ={60}
+                                    translateX={-10}
+                                    className="w-auto mt-4"
+                                >
+                                    <div className='flex items-center'>
+                                        {iconLists.map((icon, index) => (
+                                            <div 
+                                                key={icon} 
+                                                className='border border-w/[0.2] rounded-full bg-black lg:w-12 lg:h-12 w-10 h-10 flex items-center justify-center' 
+                                                style={{
+                                                    transform: `translateX(-${5 * index * 2}px)`
+                                                }}
+                                            >
+                                                <img src={icon} alt={icon} className='p-2' />
+                                            </div>
+                                        ))}
+                                    </div>
+                                </CardItem>
+                                <CardItem
+                                    translateZ={60}
+                                    translateX={10}
+                                    as="button"
+                                    className="w-auto mt-4"
+                                >
+                                    <div className='flex items-center justify-center gap-1'>
+                                        <p className='flex lg:text-xl md:text-xs text-sm text-[#a784f9] font-bold'>Check Live Site</p>
+                                        <FaLocationArrow className='ms-3' color='#a784f9'/>
+                                    </div>
+                                </CardItem>
+                            </div>
+                        </CardBody>
+                    </CardContainer>
                 </div>
             ))}
         </div>
